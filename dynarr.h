897f41115def2d9ab878b676391f7238fe1d9b86
Raihan Khan <info@abcltech.org>
2020-07-17 17:04:45 +0600

    Initial commit

---
+++ dynarr.h
@@ -0,0 +1,22 @@
+//
+// Created by Raihan Khan on 17/7/20.
+//
+
+#ifndef DYNARR_H_INCLUDED
+#define DYNARR_H_INCLUDED
+class dynArr
+{
+public:
+    dynArr();
+    dynArr(int);
+    ~dynArr();
+    void setValue(int, int);
+    int getValue(int);
+    void allocate(int);
+
+private:
+    int *data;
+    int size;
+};
+#include"dynarr.tpp"
+#endif // DYNARR_H_INCLUDED
